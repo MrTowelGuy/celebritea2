a2888b80554b615615baae986265ee19e9bd2d80
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-10-24 22:28:31 -0400

    created href links for each card

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tea(models.Model):
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    witnesses = models.IntegerField()
+
+    def __str__(self):
+        return self.title
